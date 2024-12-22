3bc09295ca52eb5894b5bcf0bb4299c319513fab
mayu.watanabe <m_popo.xx@i.softbank.jp>
2024-12-22 17:18:28 +0900

    課題です。

--- src/jp/co/kiramex/dbSample/model/Review05.java
+++ src/jp/co/kiramex/dbSample/model/Review05.java
@@ -1,62 +1,60 @@
 package jp.co.kiramex.dbSample.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
 
 
 public class Review05 {
 
     public static void main(String[] args) {
 
-           // 3. データベース接続と結果取得のための変数宣言
-           Connection con = null;
-           Statement stmt = null;
-           PreparedStatement pstmt = null; // ← 修正
-           ResultSet rs = null;
 
+            // 3. データベース接続と結果取得のための変数宣言
+            Connection con = null;
+            PreparedStatement ipstmt = null;
+            ResultSet rs = null;
 
-        try {
-
-
+    try {
             // 1. ドライバのクラスをJava上で読み込む
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // 2. DBと接続する
             con = DriverManager.getConnection(
-                "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
-                "root",
-                "watanabe1998"
-            );
+                    "jdbc:mysql://localhost/kadaidb?useSSL=false&allowPublicKeyRetrieval=true",
+                    "root",
+                    "watanabe1998"
+                );
 
-            // 4. DBとやりとりする窓口（Statementオブジェクト）の作成
+            // 4. DBとやりとりする窓口（PreparedStatementオブジェクト）の作成
             String sql = "SELECT * FROM person WHERE id = ?";    // ← 修正
-            pstmt = con.prepareStatement(sql);  // ← 修正
+            ipstmt = con.prepareStatement(sql);  // ← 修正
 
             // 5, 6. Select文の実行と結果を格納／代入
-            System.out.print("検索キーワードを入力してください > ");    // ← 追記
-            String input = keyIn();    // ← 追記
+            System.out.print("検索キーワードを入力してください > ");
+            String input = keyIn();
+            int num1 = keyInNum();
+            ipstmt.setInt(1,num1);
             // PreparedStatementオブジェクトの?に値をセット  // ← 追記
-            pstmt.setString(1, input);  // ← 追記
-            rs = pstmt.executeQuery();  // ← 修正
+
+
+            rs = ipstmt.executeQuery();  // ← 修正
 
             // 7. 結果を表示する
-            while( rs.next() ){
+            while( rs.next() ) {
                 // Name列の値を取得
                 String name = rs.getString("Name");
-                // age列の値を取得 　← 追記
-                int age = rs.getInt("age");  // ← 追記
+                // age列の値を取得
+                int age = rs.getInt("age");
                 // 取得した値を表示
                 System.out.println(name);
-                System.out.println(age);   // ← 追記
+                System.out.println(age);
             }
-
 
         } catch (ClassNotFoundException e) {
             System.err.println("JDBCドライバのロードに失敗しました。");
@@ -64,19 +62,35 @@ public class Review05 {
         } catch (SQLException e) {
             System.err.println("データベースに異常が発生しました。");
             e.printStackTrace();
-        }finally {
+        } finally {
             // 8. 接続を閉じる
-         if( con != null) {
-             try {
-                 con.close();
-             } catch (SQLException e) {
-                 System.err.println("データベース切断時にエラーが発生しました。");
-                 e.printStackTrace();
-             }
+            if( rs != null ){
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    System.err.println("ResultSetを閉じるときにエラーが発生しました。");
+                    e.printStackTrace();
+                }
+            }
+            if (ipstmt != null) {    // ← 修正
+                try {
+                    ipstmt.close();    // ← 修正
+                } catch (SQLException e) {
+                    System.err.println("PreparedStatementを閉じるときにエラーが発生しました。");    // ← 修正
+                    e.printStackTrace();
+                }
+            }
+            if( con != null ){
+                try {
+            con.close();
+        } catch (SQLException e) {
+                    System.err.println("データベース切断時にエラーが発生しました。");
+                    e.printStackTrace();
 
+            }
          }
-        }
-        }
+      }
+   }
     /*
      * キーボードから入力された値をStringで返す 引数：なし 戻り値：入力された文字列
      */
@@ -90,18 +104,17 @@ public class Review05 {
         }
         return line;
     }
+
     /*
      * キーボードから入力された値をintで返す 引数：なし 戻り値：int
      */
     private static int keyInNum() {
         int result = 0;
         try {
-            result = Integer.parseInt(keyIn());
+            result = Integer.parseInt("1");
         } catch (NumberFormatException e) {
         }
         return result;
     }
 
 }
-
-
